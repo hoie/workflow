033b19f0923699f863aa17d6d94a2afc28f2308e
Oyvind Hoie <hoie@draft.no>
2015-06-24 12:58:26 +0200

    Created default task

--- gulpfile.js
+++ gulpfile.js
@@ -10,31 +10,34 @@ var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
-	'components/scripts/template.js',
+	'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
 
-gulp.task ('coffee', function() {
+gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare: true})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts/'))
 });
 
-gulp.task ('js', function(){
+gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js/'))
 });
 
-gulp.task ('compass', function(){
+gulp.task('compass', function(){
 	gulp.src(sassSources)
 	.pipe(compass({
 		sass: 'components/sass/',
 		image: 'builds/development/images/',
-		style: 'expanded'
-	}))
-	.on('error', gutil.log)
+		style: 'expanded',
+		comments: true
+	})
+		.on('error', gutil.log))
 	.pipe(gulp.dest('builds/development/css/'))
 });
+
+gulp.task('default', ['coffee', 'js', 'compass']);
